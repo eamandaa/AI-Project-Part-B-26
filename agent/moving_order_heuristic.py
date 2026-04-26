d58343963f8db59bbaf8dac395af2eb638cdba94
JiaYiTong23 <jttong2@student.unimelb.edu.au>
2026-04-26 11:15:16 +0000

    Place-fix: change correct variable

--- agent/moving_order_heuristic.py
+++ agent/moving_order_heuristic.py
@@ -132,7 +132,7 @@ def score_push_off_board_risk(
             if not board._is_within_bounds(behind_r, behind_c):
                 break
             
-            behind = Coord(behind_c, behind_r)
+            behind = Coord(behind_r, behind_c)
             if behind not in enemy_coord:
                 continue
 
