99e137a371fb1801d1bab02900f93906fab6afa6
JiaYiTong23 <jttong2@student.unimelb.edu.au>
2026-04-26 06:33:33 +0000

    d cjange to 2

--- agent/program.py
+++ agent/program.py
@@ -119,10 +119,10 @@ class Agent:
             case PlayerColor.RED:
                 print("phase", self._board.phase)
                 #print("Testing: RED is playing a MOVE action")
-                return choose_best_action(self,self._board,depth = 3)
+                return choose_best_action(self,self._board,depth = 2)
             case PlayerColor.BLUE:
                 #print("Testing: BLUE is playing a MOVE action")
-                return choose_best_action(self,self._board,depth = 3)
+                return choose_best_action(self,self._board,depth = 2)
 
         
     def _find_cells(
